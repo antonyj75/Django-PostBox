2e47a8d77992364b89a75980b5b874f0cc21a05d
Antony J <antonyj75@yahoo.com>
2013-07-30 05:06:56 +0530

    Adding test folder with website files in it to help test the PostBox webapp.

---
+++ test/PostalService/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+#from PostBox import views
+
+urlpatterns = patterns('',
+    # url(r'^PostBox/', include('PostBox.urls')),
+    url(r'^$', include('PostBox.urls', namespace='PostBox')),
+    # Examples:
+    # url(r'^$', 'PostBox.views.index', name='index'),
+    # url(r'^PostalService/', include('PostalService.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
